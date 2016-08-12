d29b59b75f7600f0f7715f06684521299fedd680
DESKTOP-TK4H2RO\wangyong <wangyongyeah@126.com>
2016-08-12 16:09:29 +0800

    '项目初始化'

---
+++ web/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangyong on 2016/8/12.
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello,springBoot...";
+    }
+}
